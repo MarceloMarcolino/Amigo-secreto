62b90985ba0e71cde01b9fff69411475ef57aa9c
Marcelo Antonio Pereira Marcolino <marcelo.marcolino.dev@gmil.com>
2025-07-17 00:00:22 -0300

    Realizando o sorteio

--- js/app.js
+++ js/app.js
@@ -14,16 +14,23 @@ function adicionar(){
 
 function sortear() {
     embaralha(amigos);
+    let sorteio = document.getElementById('lista-sorteio');
+
+    for (let i = 0; i < amigos.length; i++) {
+
+        if (i == amigos.length - 1) { 
+            sorteio.innerHTML = sorteio.innerHTML + amigos[i] + ' --> ' + amigos[0] + '<br>';  
+        } else {
+            sorteio.innerHTML = sorteio.innerHTML + amigos[i] + ' --> ' + amigos[i + 1] + '<br>';  
+        }
+    }
 }
 
 function embaralha(lista) {
-
     for (let indice = lista.length; indice; indice--) {
-
         const indiceAleatorio = Math.floor(Math.random() * indice);
 
         // atribuição via destructuring
-        [lista[indice - 1], lista[indiceAleatorio]] = 
-            [lista[indiceAleatorio], lista[indice - 1]];
+        [lista[indice - 1], lista[indiceAleatorio]] = [lista[indiceAleatorio], lista[indice - 1]];
     }
 }
