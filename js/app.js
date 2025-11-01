c2c52e93a26b5b383915164a82982fe6b305da8e
Marcelo Antonio Pereira Marcolino <marcelo.marcolino.dev@gmil.com>
2025-11-01 17:33:45 -0300

    Desafio 11: Crie uma função que receba um array de números e retorne a soma dos elementos pares e o produto dos elementos ímpares.

--- js/app.js
+++ js/app.js
@@ -260,3 +260,28 @@ if (objetoEstaPresente) {
 } else {
     console.log("O aluno não está presente no array.");
 }
+
+// Desafio 11
+
+function calcularSomaProduto(array) {
+    let somaPares = 0;
+    let produtoImpares = 1;
+
+    for (let numero of array) {
+        if (numero % 2 === 0) {
+            somaPares += numero;
+        } else {
+            produtoImpares *= numero;
+        }
+    }
+
+    return {
+        somaPares,
+        produtoImpares
+    };
+}  
+
+const numeros = [1, 2, 3, 4, 5];
+const resultado = calcularSomaProduto(numeros);
+console.log("Soma dos pares:", resultado.somaPares);
+console.log("Produto dos ímpares:", resultado.produtoImpares);
